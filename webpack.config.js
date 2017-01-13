794d935d7ddeae0bd693782cf8a97bf31b3d1a71
云煦 <weiyun.zwy@alibaba-inc.com>
2017-01-13 09:38:53 +0800

    webpack

---
+++ webpack.config.js
@@ -0,0 +1,17 @@
+const webpack = require('webpack');
+
+module.exports = {
+  entry: './src/entry.js',
+  output: {
+    path: __dirname,
+    filename: './dist/bundle.js'
+  },
+  module: {
+    loaders: [
+      {test: /\.css$/, loader: 'style!css'}
+    ]
+  },
+  plugins: [
+    new webpack.BannerPlugin('This file is created by zouweiyun')
+  ]
+}
